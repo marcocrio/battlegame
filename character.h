4472d77b2d932b1a08d02ea90100288e32950cc4
marcocrio <maramirez25@miners.utep.edu>
2018-11-17 18:17:17 -0700

    Created Character's class

---
+++ character.h
@@ -0,0 +1,14 @@
+class character {
+    private:
+        string name;
+        string health;
+        string attack_strenght;
+        string luck;
+        string speed;
+        string defense;
+    
+    public:
+
+
+
+}
