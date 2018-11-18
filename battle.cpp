872b95b8cb2f86b291453719fd704575d54e7342
marcocrio <maramirez25@miners.utep.edu>
2018-11-17 18:45:57 -0700

    run test

--- battle.cpp
+++ battle.cpp
@@ -1,6 +1,43 @@
 
 #include <iostream>
-#include "character.h"
+
+class Character {
+    private:
+        string  name;
+        int     health;
+        int     attack_strenght;
+        int     luck;
+        int     speed;
+        int     defense;
+    
+    public:
+        Character(
+            string  iname    = "Not defined", 
+            int     ihealth  = "Not defined", 
+            int     iattack  = "Not defined", 
+            int     iluck    = "Not defined",
+            int     ispeed   = "Not defined",
+            int     idefense = "Not defined"):name(iname),health(ihealth),attack_strenght(iattack),luck(iluck),defense(idefense);
+
+        void attr();
+
+}
+
+
+void Character::attr(){
+    cout    << name 
+            << " " 
+            << health
+            << " " 
+            << attack_strenght
+            << " " 
+            << luck
+            << " " 
+            << speed
+            << " " 
+            << defense
+            
+}
 
 
 int main() {
