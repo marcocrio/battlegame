d95405dcb5f5101550f06d28d2db1de39cc9fb74
marcocrio <maramirez25@miners.utep.edu>
2018-11-17 20:53:57 -0700

    Branch testing

--- battle.cpp
+++ battle.cpp
@@ -5,12 +5,9 @@ using namespace std;
 #include ".include/character.h"
 
 
-
-
-
 int main() {
 
-    Character marco("Marco", 1000,50,10,5,30);
+    Character marco("Charlie", 1000,50,10,5,30);
 
     marco.attr();
 
